c34b69454efee3ad2106e9edcc23692dd83748cc
Aylla Moreira <ayllatyfanny16@gmail.com>
2022-12-23 21:41:19 -0300

    modal transações e listagem dados api mirage

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'frela',
+          category: 'dev',
+          type: 'deposit',
+          amount: 6000,
+          createdAt: new Date('2022-12-23 21:00:00')
+        },
+        {
+          id: 2,
+          title: 'compras',
+          category: 'comida',
+          type: 'withdraw',
+          amount: 600,
+          createdAt: new Date('2022-12-21 09:00:00')
+        }
+      ]
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    });
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
